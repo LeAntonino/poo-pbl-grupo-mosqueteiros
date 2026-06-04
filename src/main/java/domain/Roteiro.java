77d63e45eef5cac309a0d141e6a09956a9836017
Antonino <antoninomenegassi@gmail.com>
2026-06-04 19:18:29 -0300

    Testes Criados

--- src/main/java/domain/Roteiro.java
+++ src/main/java/domain/Roteiro.java
@@ -4,9 +4,9 @@ package domain;
 public class Roteiro {
     
     private final String nome;
-    private final float distancia;
+    private final double distancia;
     
-    public Roteiro(String nome, float distancia) {
+    public Roteiro(String nome, double distancia) {
         this.nome = nome;
         this.distancia = distancia;
 
@@ -16,7 +16,7 @@ public class Roteiro {
         return nome;
     }
 
-    public float getDistancia() {
+    public double getDistancia() {
         return distancia;
     }  
     
--- src/main/java/presentation/main.java
+++ src/main/java/presentation/main.java
@@ -11,12 +11,6 @@ public class main {
 
     public static void main(String[] args) {
 
-        Eletronico e = new Eletronico("computador", 3);
-        Roteiro r = new Roteiro("jaragua", 20);
-        Frota f = new Frota("os caras de pau", 9);
-
-        Intinerario inti = new Intinerario(r, f, e);
-
         Scanner sc = new Scanner(System.in);
         int opcao;
 
